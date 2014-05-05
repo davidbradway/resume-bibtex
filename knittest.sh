8fcb35421c23c4aa9a4d1a9b47ee16f077512158
David Bradway <david.bradway@gmail.com>
2014-05-05 13:49:08 +0200

    remove files made obselete

--- knittest.sh
+++
@@ -1,5 +0,0 @@
-# Make the manuscript
-pandoc -H margins.sty --bibliography test.bib --csl plos.csl test.md -o test.pdf
-
-# Make TeX files
-pandoc -H margins.sty --bibliography test.bib --csl plos.csl test.md -o test.tex
