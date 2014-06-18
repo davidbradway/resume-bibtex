2cc6c748a048056841307ccb5dca95187869bd40
David Bradway <david.bradway@gmail.com>
2014-06-18 14:44:08 +0200

    remove figure list (used in paper tempate)

--- knit.sh
+++ knit.sh
@@ -1,16 +1,7 @@
 ./clean.sh
 
-# Make a list of figures that can be appended
-#pandoc -H margins.sty list_of_figures.md -o list_of_figures.pdf
 # Make the manuscript
 pandoc -H margins.sty --bibliography mendeley.bib --csl plos.csl resume_with_citations.md -o resume.pdf
-# Combine the two using PDFtk
-#pdftk resume.pdf list_of_figures.pdf cat output combined.pdf
-# Remove duplicate files and rename concated one to original name
-#rm list_of_figures.pdf -f
-#rm resume.pdf -f
-#mv combined.pdf resume.pdf
 
 # Make TeX files
 pandoc -H margins.sty --bibliography mendeley.bib --csl plos.csl resume_with_citations.md -o resume.tex
-#pandoc -H margins.sty list_of_figures.md -o list_of_figures.tex
