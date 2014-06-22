d5c0eb7088d410f7da70971a68acd7a5ace42ca7
David Bradway <david.bradway@gmail.com>
2014-06-22 16:09:41 +0200

    Test MS Word Docx support on Windows

--- knit.sh
+++ knit.sh
@@ -5,3 +5,6 @@ pandoc -H margins.sty --bibliography mendeley.bib --csl plos.csl resume_with_cit
 
 # Make TeX files
 pandoc -H margins.sty --bibliography mendeley.bib --csl plos.csl resume_with_citations.md -o resume.tex
+
+# Make Word Doc
+pandoc -H margins.sty --bibliography mendeley.bib --csl plos.csl resume_with_citations.md -o resume.docx
